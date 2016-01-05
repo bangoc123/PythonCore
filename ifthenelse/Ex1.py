789c9fa65dfa0e502e8c0526540a6d46a7479197
bangoc123 <bangoc123>
2016-01-05 14:37:48 +0700

    Ex1

---
+++ ifthenelse/Ex1.py
@@ -0,0 +1,12 @@
+score = 70
+
+if score >= 90:
+	print("grae is A")
+elif score >= 80:
+	print("grade is B")
+elif score >= 70:
+	print("grade is C")
+elif score >= 60:
+	print("grade is D")
+else:
+	print("grade is F")

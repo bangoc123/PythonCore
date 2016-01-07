7193bed54723a0283328f42f1870f7c2243fb5ca
bangoc123 <bangoc123>
2016-01-07 11:59:07 +0700

    Update code Ex1

---
+++ whileLoop/Ex1.py
@@ -0,0 +1,28 @@
+#gcd = GreatestCommonDivisor
+
+a = int(input("Enter first integer: "))
+
+b = int(input("Enter second integer: ")) 
+
+#min = 0
+
+if a == 0 and b == 0:
+	print("Do not exsist divisor")
+elif a!=0 and b == 0:
+	print('The greatest common divison for',a,'and',b,'is',abs(a))
+elif a==0 and b != 0:
+	print('The greatest common divion for',a,'and',b,'is',abs(b))
+else:
+	if a<b: 
+		min = a
+	else:
+		min = b
+
+	gcd = min
+
+	while gcd <= min:
+		if(a%gcd ==0 and b%gcd ==0):
+			print('The greatest common divison for',a,'and',b,'is',gcd)
+			break
+		gcd -= 1
+

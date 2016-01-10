c405130b14e0915b62151e0ca09424764dcdb093
bangoc123 <bangoc123>
2016-01-10 17:02:25 +0700

    Dictionary

---
+++ dictionary/Ex.py
@@ -0,0 +1,27 @@
+keys = {'a':'n', 'b':'o', 'c':'p', 'd':'q', 'e':'r', 'f':'s', 'g':'t', 'h':'u', 
+       'i':'v', 'j':'w', 'k':'x', 'l':'y', 'm':'z', 'n':'a', 'o':'b', 'p':'c', 
+       'q':'d', 'r':'e', 's':'f', 't':'g', 'u':'h', 'v':'i', 'w':'j', 'x':'k',
+       'y':'l', 'z':'m', 'A':'N', 'B':'O', 'C':'P', 'D':'Q', 'E':'R', 'F':'S', 
+       'G':'T', 'H':'U', 'I':'V', 'J':'W', 'K':'X', 'L':'Y', 'M':'Z', 'N':'A', 
+       'O':'B', 'P':'C', 'Q':'D', 'R':'E', 'S':'F', 'T':'G', 'U':'H', 'V':'I', 
+       'W':'J', 'X':'K', 'Y':'L', 'Z':'M'}
+
+message = input("Type the encoded message: ")
+
+list1 = []
+	
+for char in message:
+	if char not in keys.keys():
+		list1.append(char)
+	
+	else:			
+		for key in keys.keys():
+			if keys[key] == char:
+				list1.append(key)			
+
+real_message = ''.join(list1)
+
+print("The real message is: ",real_message)
+	
+
+
